dd8c8109526f4c5b8939e57b23fa281715050c20
dungptfx13091 <dungptfx13091@funix.edu.vn>
2023-10-01 11:37:08 +0900

    llkjdhhgga

--- app.js
+++ app.js
@@ -26,7 +26,9 @@ app.use(
     credentials: true,
   })
 );
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(userRouter);
 app.use(productRouter);
